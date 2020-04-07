cfa48e118a31a3be2a10904fdac7974e74297638
Drouin <Robert.Drouin@vantiv.com>
2020-04-07 16:53:32 -0400

    Initial commit.

---
+++ Apps/phbitsight/bitsight_consts.py
@@ -0,0 +1,8 @@
+# Define your constants here
+BITSIGHT_API_PATH = '/ratings'
+BITSIGHT_ALERTS_ENDPOINT = '/v2/alerts/'
+BITSIGHT_ALERTS_LATEST_ENDPOINT = '/v2/alerts/latest'
+BITSIGHT_GET_ALERT_ENDPOINT = '/v1/alerts/percent/{alert_guid}'
+BITSIGHT_LIST_COMPANIES_ENDPOINT = '/v1/portfolio/'
+BITSIGHT_GET_COMPANY_ENDPOINT = '/v1/companies/{company_guid}'
+BITSIGHT_NEWS_ENDPOINT = '/v1/news'
